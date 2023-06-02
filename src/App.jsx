ba6a0116676e47abff10363263f561148487be52
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-06-02 10:31:24 +0530

    onClick invokes onSquareClicks which invokes handleClick, this handles the value setting to X

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,25 @@ function Square({value, onSquareClick}) {
 }
 function Board(){
   const  [squares, setSquares] = useState(Array(9).fill(null))
-  function handleClick(){
-    
+  function handleClick(i){
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
   }
   return(
     <>
       <div className='board-row'></div>
-      <Square value={squares[0]} onSquareClick={handleClick}></Square>
-      <Square value={squares[1]} onSquareClick={handleClick}></Square>
-      <Square value={squares[2]} onSquareClick={handleClick}></Square>
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)}></Square>
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)}></Square>
       <div className='board-row'></div>
-      <Square value={squares[3]} onSquareClick={handleClick}></Square>
-      <Square value={squares[4]} onSquareClick={handleClick}></Square>
-      <Square value={squares[5]} onSquareClick={handleClick}></Square>
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)}></Square>
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)}></Square>
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)}></Square>
       <div className='board-row'></div>
-      <Square value={squares[6]} onSquareClick={handleClick}></Square>
-      <Square value={squares[7]} onSquareClick={handleClick}></Square>
-      <Square value={squares[8]} onSquareClick={handleClick}></Square>
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)}></Square>
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)}></Square>
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)}></Square>
       
     </>
   )
