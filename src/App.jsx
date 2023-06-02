378e26859d653d35b3611414f2e67f95aad4f890
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-06-02 13:18:33 +0530

    game's working,program stops when any player wins.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,39 @@
 import { useState } from 'react'
 import './App.css'
 
+function findWinner(squares){
+  const winningCombos=[//the purpose of making this to finding winning combo and we would compare it with squares values
+  // we are checking if the combination exist or not with the help of indexes of these arrays inside winningCombos array
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  /*
+  // for winning condition only for winningCombos[0] inputs,
+  const a = winningCombos[0][0]; // 0
+  const b = winningCombos[0][1]; // 1
+  const c = winningCombos[0][2]; // 2
+  if(squares[a]==squares[b] && squares[a]==squares[c]){ //implies squares[0]==squares[1] && squares[0]==squares[2]
+    return squares[a];
+  }
+  */
+
+  // for executing all winningCombos inputs,
+  for (let i = 0; i < winningCombos.length; i++) {
+    const [a, b, c] = winningCombos[i];
+    if(squares[a]==squares[b] && squares[a]==squares[c]){ //in above we only did for index 0 of winning combos, but now we do it for every index of it.
+    return squares[a];
+    }
+  }
+  return null;
+}
+
+
 function Square({value, onSquareClick}) {
   return(
     <>
@@ -11,15 +44,26 @@ function Square({value, onSquareClick}) {
   )
 }
 function Board(){
+
   const  [squares, setSquares] = useState(Array(9).fill(null))
-  const [isX , setX] = useState(true);//rule 1: first input should be X
+
+  const [isX , setX] = useState(true);//rule : first input should be X
+
   function handleClick(i){
-    if(squares[i]!==null){// this will ensure once value is intialized as X or O it should not be re-intialized with opposite value 
+
+    if(squares[i]!==null){// checks if onClick there something already exist or not
+      //this will ensure once value is intialized as X or O it should not be switched with opposite value 
       return;
     }
-    if(squares[i]!==null){
+    if(findWinner(squares)){//every time handleClick is invoked, through if statement findWinner function is invoked it tells if winning combo is yet made or not
+      // on false which is by default, new inputs can be made so that winning condition can be made
+      // on true, winning condition is satisfied and we use return to terminate the handleClick function from further execution
+      // on every click either the function is terminated because of click on already existing input
+      // or the winning combo is made which doesnot let any new input to take because function would be terminated beforehand updating the state
+      // again, since findWinner if statement would return/terminate the function to be blunt.
       return;
     }
+
     const nextSquares = squares.slice();
     if(isX){
       //since is X by default true always this will execute first
@@ -30,7 +74,9 @@ function Board(){
       setX(true);//this will invoke if statement to be executed on click after this
     }
     setSquares(nextSquares);
+    
   }
+
   return(
     <>
       <div className='board-row'></div>
@@ -49,9 +95,6 @@ function Board(){
     </>
   )
 }
-
-
-
 
 function App() {
   return (
