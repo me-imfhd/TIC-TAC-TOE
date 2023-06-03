d7b51c1ec37451cb0f8f5997a01ca8e31222f05f
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-06-03 11:34:59 +0530

    removed comments

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import "./App.css";
 
 function findWinner(squares) {
   const winningCombos = [
-    //the purpose of making this to finding winning combo and we would compare it with squares values
-    // we are checking if the combination exist or not with the help of indexes of these arrays inside winningCombos array
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -30,7 +28,6 @@ function findWinner(squares) {
   for (let i = 0; i < winningCombos.length; i++) {
     const [a, b, c] = winningCombos[i];
     if (squares[a] == squares[b] && squares[a] == squares[c]) {
-      //in above we only did for index 0 of winning combos, but now we do it for every index of it.
       return squares[a];
     }
   }
@@ -47,32 +44,23 @@ function Square({ value, onSquareClick }) {
 function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
-  const [isX, setX] = useState(true); //rule : first input should be X
+  const [isXNext, setX] = useState(true); 
 
   function handleClick(i) {
     if (squares[i] !== null) {
-      // checks if onClick there something already exist or not
-      //this will ensure once value is intialized as X or O it should not be switched with opposite value
       return;
     }
     if (findWinner(squares)) {
-      //every time handleClick is invoked, through if statement findWinner function is invoked it tells if winning combo is yet made or not
-      // on false which is by default, new inputs can be made so that winning condition can be made
-      // on true, winning condition is satisfied and we use return to terminate the handleClick function from further execution
-      // on every click either the function is terminated because of click on already existing input
-      // or the winning combo is made which doesnot let any new input to take because function would be terminated beforehand updating the state
-      // again, since findWinner if statement would return/terminate the function to be blunt.
       return;
     }
 
     const nextSquares = squares.slice();
-    if (isX) {
-      //since is X by default true always this will execute first
+    if (isXNext) {
       nextSquares[i] = "X";
-      setX(false); // this will invoke else statement to be executed on click after this
+      setX(false); 
     } else {
       nextSquares[i] = "O";
-      setX(true); //this will invoke if statement to be executed on click after this
+      setX(true);
     }
     setSquares(nextSquares);
   }
@@ -95,7 +83,7 @@ function Board() {
             </button>
           </>
         ) : (
-          <h3>Player {isX ? <span>X</span> : <span>O</span>} move</h3>
+          <h3>Player {isXNext ? <span>X</span> : <span>O</span>} move</h3>
         )}
       </div>
       <div className="board-row"></div>
@@ -115,10 +103,9 @@ function Board() {
 }
 
 function App() {
-  return (
-    <>
-      <Board></Board>
-    </>
+  
+   return (  
+    <Board />
   );
 }
 
